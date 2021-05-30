141b3d99b7f47fe863bea7cfa65e0dc931705d19
Ranjit Yadav <yadavranjit521@gmail.com>
2021-05-30 18:13:00 +0530

    Main Activity and other fragments added with bottom nav view

--- app/src/main/java/com/strangecoder/socialmedia/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/strangecoder/socialmedia/ui/auth/AuthActivity.kt
@@ -1,8 +1,11 @@
 package com.strangecoder.socialmedia.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.strangecoder.socialmedia.R
+import com.strangecoder.socialmedia.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +13,12 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/strangecoder/socialmedia/ui/main/fragments/EditProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.strangecoder.socialmedia.ui.main.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.strangecoder.socialmedia.databinding.FragmentEditProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditProfileFragment : Fragment() {
+
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
+//    private val viewModel: AuthViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
