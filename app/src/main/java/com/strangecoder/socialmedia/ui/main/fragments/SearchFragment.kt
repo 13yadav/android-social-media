391f3781e1480b96740abfe53772616095f7cfe2
Ranjit Yadav <yadavranjit521@gmail.com>
2021-06-13 22:13:24 +0530

    added:Search user fragment

--- app/src/main/java/com/strangecoder/socialmedia/ui/main/fragments/SearchFragment.kt
+++ app/src/main/java/com/strangecoder/socialmedia/ui/main/fragments/SearchFragment.kt
@@ -4,16 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.strangecoder.socialmedia.databinding.FragmentSearchBinding
+import com.strangecoder.socialmedia.other.Constants.SEARCH_TIME_DELAY
+import com.strangecoder.socialmedia.other.EventObserver
+import com.strangecoder.socialmedia.ui.main.adapters.UserAdapter
+import com.strangecoder.socialmedia.ui.main.viewmodels.SearchViewModel
+import com.strangecoder.socialmedia.ui.viewutils.snackBar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
-//    private val viewModel: AuthViewModel by viewModels()
+
+    @Inject
+    lateinit var userAdapter: UserAdapter
+
+    private val viewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +45,44 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupRecyclerView()
         subscribeToObservers()
+
+        var job: Job? = null
+        binding.etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(SEARCH_TIME_DELAY)
+                editable?.let {
+                    viewModel.searchUser(it.toString())
+                }
+            }
+        }
+
+        userAdapter.setOnUserClickListener { user ->
+            findNavController()
+                .navigate(SearchFragmentDirections.actionGlobalToOthersProfileFragment(user.uid))
+        }
     }
 
     private fun subscribeToObservers() {
+        viewModel.searchResults.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                binding.searchProgressBar.isVisible = false
+                snackBar(it)
+            },
+            onLoading = {
+                binding.searchProgressBar.isVisible = true
+            }
+        ) { users ->
+            binding.searchProgressBar.isVisible = false
+            userAdapter.users = users
+        })
+    }
 
+    private fun setupRecyclerView() = binding.rvSearchResults.apply {
+        adapter = userAdapter
+        itemAnimator = null
     }
 
     override fun onDestroyView() {
